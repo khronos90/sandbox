eb4b83753081a4bf1ab3929a8518dc13f107b6a2
Bruno Fleischer <khronos90@gmail.com>
2022-07-01 19:26:36 -0300

    Added dotenv typing and base server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,10 @@
-function linterTest(aNumber: number) {
-  console.log("This is a message", aNumber)
-}
+import express from 'express';
+import dotenv from 'dotenv'
+dotenv.config();
 
-linterTest(10);
+const app = express();
+const port = 3001;
+
+app.listen(port, async () => {
+  console.log(`App started at port ${port}`)
+});
---
+++ server/src/types/env.d.ts
@@ -0,0 +1,12 @@
+export { };
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB_USER: string;
+      DB_PASSWORD: string;
+      HOST: string;
+      ENV: string;
+    }
+  }
+}
