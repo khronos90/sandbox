92fb7314c0b97cee5fa6c174d5836b683135a46c
Bruno Fleischer <khronos90@gmail.com>
2022-07-02 13:54:13 -0300

    Cleaned router

--- server/src/controllers/tutorials/index.ts
+++ server/src/controllers/tutorials/index.ts
@@ -1,36 +1,42 @@
-import Tutorial, { ITutorialInput, ITutorialOutput } from '../../db/models/tutorial';
+import { Request, Response } from 'express';
+import Tutorial, { ITutorialInput } from '../../db/models/tutorial';
+import { TypedRequestBody } from '../../types/types';
 
 export class TutorialController {
-  static async getTutorials(): Promise<Tutorial[]> {
+  static async getTutorials(req: Request, res: Response) {
     const tutorials = await Tutorial.findAll();
-    return tutorials;
+    res.send(tutorials);
   }
 
-  static async getTutorial(id: string): Promise<Tutorial | null> {
-    const tutorial = await Tutorial.findByPk(id);
-    return tutorial;
+  static async getTutorial(req: Request, res: Response) {
+    const tutorial = await Tutorial.findByPk(req.params.id);
+    res.send(tutorial);
   }
 
-  static async createTutorial(tutorial: ITutorialInput): Promise<Tutorial> {
+  static async createTutorial(req: TypedRequestBody<ITutorialInput>, res: Response) {
+    const tutorial: ITutorialInput = req.body;
     const newTutorial = await Tutorial.create(tutorial);
-    return newTutorial;
+    res.send(newTutorial);
   }
 
-  static async deleteTutorial(id: string): Promise<void> {
-    const tutorial = await Tutorial.findByPk(id);
-    return tutorial?.destroy();
+  static async deleteTutorial(req: Request, res: Response): Promise<void> {
+    const tutorial = await Tutorial.findByPk(req.params.id);
+    tutorial?.destroy();
+    res.send('deleted');
   }
 
-  static async updateTutorial(id: string, tutorial: ITutorialInput): Promise<Tutorial | null> {
-    const tutorialToUpdate = await Tutorial.findByPk(id);
+  static async updateTutorial(req: TypedRequestBody<ITutorialInput>, res: Response) {
+    const tutorial: ITutorialInput = req.body;
+    const tutorialToUpdate = await Tutorial.findByPk(req.params.id);
     tutorialToUpdate?.update(tutorial);
-    return tutorialToUpdate;
+    res.send(tutorialToUpdate);
   }
 
-  static deleteAllTutorials() {
+  static deleteAllTutorials(req: Request, res: Response) {
     Tutorial.destroy({
       where: {},
       truncate: true,
     });
+    res.send('DELETED');
   }
 }
--- server/src/routes/tutorials/index.ts
+++ server/src/routes/tutorials/index.ts
@@ -5,36 +5,16 @@ import { TypedRequestBody } from '../../types/types';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const tutorials = await TutorialController.getTutorials();
-  res.send(tutorials);
-});
+router.get('/', TutorialController.getTutorials);
 
-router.get('/:id', async (req, res) => {
-  const tutorial = await TutorialController.getTutorial(req.params.id);
-  res.send(tutorial);
-});
+router.get('/:id', TutorialController.getTutorial);
 
-router.post('/', async (req, res) => {
-  const tutorial: ITutorialInput = req.body;
-  const newTutorial = await TutorialController.createTutorial(tutorial);
-  res.send(newTutorial);
-});
+router.post('/', TutorialController.createTutorial);
 
-router.put('/:id', async (req: TypedRequestBody<ITutorialInput>, res) => {
-  const tutorial: ITutorialInput = req.body;
-  const newTutorial = await TutorialController.updateTutorial(req.params.id, tutorial);
-  res.send(newTutorial);
-});
+router.put('/:id', TutorialController.updateTutorial);
 
-router.delete('/mass_delete/', async (req, res) => {
-  await TutorialController.deleteAllTutorials();
-  res.send('bye all');
-});
+router.delete('/mass_delete/', TutorialController.deleteAllTutorials);
 
-router.delete('/:id', async (req, res) => {
-  await TutorialController.deleteTutorial(req.params.id);
-  res.send('deleted');
-});
+router.delete('/:id', TutorialController.deleteTutorial);
 
 export default router;
